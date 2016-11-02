8b4dad5c8eda72b99f824b6535f7d4197c298d3d
Дима <dzirtdourden4@gmail.com>
2016-11-02 02:43:22 +0200

    Fifth commit

---
+++ src/FifthClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Дима on 02.11.2016.
+ */
+public class FifthClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на java");
+    }
+}
